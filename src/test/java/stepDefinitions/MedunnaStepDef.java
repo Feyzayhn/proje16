17d86d69779fcdb492eb8554cda5b02612e8f145
cevik <suleymancevik64@gmail.com>
2022-11-18 03:28:46 +0300

    22-11-18

--- src/test/java/stepDefinitions/MedunnaStepDef.java
+++ src/test/java/stepDefinitions/MedunnaStepDef.java
@@ -4,14 +4,14 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.poi.ss.usermodel.*;
 import org.junit.Assert;
-import org.openqa.selenium.Keys;
 import pages.MedunnaPageS;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
 
-import java.io.IOException;
+import java.io.*;
 
 public class MedunnaStepDef {
 
@@ -28,8 +28,8 @@ public class MedunnaStepDef {
 
         page.accountMenu.click();
         page.signIn.click();
-        page.usernameBox.sendKeys(ConfigReader.getProperty("patientUsername"));
-        page.passwordBox.sendKeys(ConfigReader.getProperty("patientPassword"));
+        page.usernameBox.sendKeys(ConfigReader.getProperty("userUsername"));
+        page.passwordBox.sendKeys(ConfigReader.getProperty("userPassword"));
         page.signInButton.click();
 
         // Actions Yontem
@@ -50,23 +50,24 @@ public class MedunnaStepDef {
     @And("kullanici current password girer")
     public void kullaniciCurrentPasswordGirer() {
 
-        page.currentPasswordBox.sendKeys(ConfigReader.getProperty("patientPassword"));
+        page.currentPasswordBox.sendKeys(ConfigReader.getProperty("userPassword"));
     }
 
     @And("kullanici new password girer")
     public void kullaniciNewPasswordGirer() {
 
-        page.newPasswordBox.sendKeys(ConfigReader.getProperty("patientPassword"));
+        page.newPasswordBox.sendKeys(ConfigReader.getProperty("userPassword"));
     }
 
     @And("kullanici new password confirmation girer")
     public void kullaniciNewPasswordConfirmationGirer() {
 
-        page.confirmPasswordBox.sendKeys(ConfigReader.getProperty("patientPassword"));
+        page.confirmPasswordBox.sendKeys(ConfigReader.getProperty("userPassword"));
     }
 
     @And("kullanici {string} mesajinin gorunur olmadigini dogrular")
     public void kullaniciMesajininGorunurOlmadiginiDogrular(String message) throws IOException {
+
 
         try {
             Assert.assertFalse(page.notMatchTextMessage.isDisplayed());
@@ -74,20 +75,26 @@ public class MedunnaStepDef {
             ReusableMethods.waitFor(2);
             ReusableMethods.getScreenshotWebElement("SSpasword", page.passwordScreen);
         }
+
     }
 
     @And("sayfayi kapatir")
     public void sayfayiKapatir() {
 
-        Driver.quitDriver();
+        Driver.closeDriver();
     }
 
+    @And("kullanici buyuk harf,rakam ve ozel karakter ile en az yedi karakter new password girer")
+    public void kullaniciBuyukHarfRakamVeOzelKarakterIleEnAzYediKarakterNewPasswordGirer() throws IOException {
+
+        page.newPasswordBox.sendKeys("AS1222!!");
+    }
 
     @And("kullanici Password strenght seviyesinin limegreen oldugunu dogrular")
     public void kullaniciPasswordStrenghtSeviyesininLimegreenOldugunuDogrular() throws IOException {
 
-        Assert.assertEquals(4,page.limeGreenStrength.size());
-        ReusableMethods.getScreenshotWebElement("limegreenSS",page.strengthBar);
+        Assert.assertEquals(4, page.limeGreenStrength.size());
+        ReusableMethods.getScreenshotWebElement("limegreenSS", page.strengthBar);
     }
 
     @And("kullanici New password'a kucuk harf ekler")
@@ -99,14 +106,8 @@ public class MedunnaStepDef {
     @And("kullanici Password strength seviyesinin green oldugunu dogrular")
     public void kullaniciPasswordStrengthSeviyesininGreenOldugunuDogrular() throws IOException {
 
-        Assert.assertEquals(5,page.greenStrength.size());
-        ReusableMethods.getScreenshotWebElement("SSgreen",page.strengthBar);
-    }
-
-    @And("kullanici buyuk harf,rakam ve ozel karakter ile en az yedi karakter new password girer")
-    public void kullaniciBuyukHarfRakamVeOzelKarakterIleEnAzYediKarakterNewPasswordGirer() {
-
-        page.newPasswordBox.sendKeys("AS1222!!");
+        Assert.assertEquals(5, page.greenStrength.size());
+        ReusableMethods.getScreenshotWebElement("SSgreen", page.strengthBar);
     }
 
     @And("kullanici kucuk harf,rakam ve ozel karakter ile en az yedi karakter new password girer")
@@ -121,21 +122,167 @@ public class MedunnaStepDef {
         page.newPasswordBox.sendKeys("BB");
     }
 
-    @And("kullanici {int} bekler")
-    public void kullaniciBekler(int saniye) {
 
-        ReusableMethods.waitFor(saniye);
-    }
-
-    @And("kullanici kucuk harf,rakam ve ozel karakter ile en az yedi karakter {string} girer")
-    public void kullaniciKucukHarfRakamVeOzelKarakterIleEnAzYediKarakterGirer(String password) {
+    @And("kullanici kucuk harf,buyuk harf,rakam ve ozel karakter sartlarindan herhangi ucu ile{string} girer")
+    public void kullaniciKucukHarfBuyukHarfRakamVeOzelKarakterSartlarindanHerhangiUcuIleGirer(String password) {
 
         page.newPasswordBox.sendKeys(password);
     }
 
-    @And("kullanici {string} buyuk harf ekler")
-    public void kullaniciBuyukHarfEkler(String karakter) {
+    @And("kullanici {string} girmedigi sifre sartini ekler")
+    public void kullaniciGirmedigiSifreSartiniEkler(String karakter) {
 
         page.newPasswordBox.sendKeys(karakter);
+    }
+
+    @When("kullanici username {string} girer")
+    public void kullaniciUsernameGirer(String username) {
+
+        page.accountMenu.click();
+        page.signIn.click();
+
+        page.usernameBox.sendKeys(username);
+    }
+
+    @When("kullanici password {string} girer")
+    public void kullaniciPasswordGirer(String password) {
+
+        page.passwordBox.sendKeys(password);
+    }
+
+    @And("kullanici {int} saniye bekler")
+    public void kullaniciSaniyeBekler(int sn) {
+
+        ReusableMethods.waitFor(sn);
+    }
+
+    @And("kullanici kucuk harf,buyuk harf,rakam ve ozel karakter ile en az yedi karakterli new password girer")
+    public void kullaniciKucukHarfBuyukHarfRakamVeOzelKarakterIleEnAzYediKarakterliNewPasswordGirer() {
+
+        page.newPasswordBox.sendKeys("javA011!!");
+    }
+
+    //String sifre = "Team16kullanici!";
+    String sifre = "Kullaniciteam16!";
+    //String eskiSifre = "Kullaniciteam16!";
+    String eskiSifre = "Team16kullanici!";
+
+    @And("kullanici mevcut sifreyi current password box'a girer")
+    public void kullaniciMevcutSifreyiCurrentPasswordBoxAGirer() {
+
+        page.currentPasswordBox.sendKeys(sifre);
+    }
+
+    @And("kullanici new password box'a eski sifreyi girer")
+    public void kullaniciNewPasswordBoxAEskiSifreyiGirer() {
+
+        page.newPasswordBox.sendKeys(eskiSifre);
+    }
+
+    @And("kullanici new password confirmation box'a ayni sifreyi girer")
+    public void kullaniciNewPasswordConfirmationBoxAAyniSifreyiGirer() {
+
+        page.confirmPasswordBox.sendKeys(eskiSifre);
+    }
+
+    @And("kullanici save butonuna tiklar")
+    public void kullaniciSaveButonunaTiklar() {
+
+
+        page.saveButton.click();
+    }
+
+    @And("kullanici Password changed! uyarisinin gorunur olmadigini dogrular")
+    public void kullaniciPasswordChangedUyarisininGorunurOlmadiginiDogrular() {
+
+        try {
+            Assert.assertFalse(page.passwordChangedMessage.isDisplayed());
+
+        } catch (Exception e) {
+
+            System.out.println("Eski sifre kullanilabildi");
+        }
+
+    }
+
+    @Given("excel test")
+    public void excelTest() throws IOException {
+
+        String filePath = "src/resources/sifre.xlsx";
+        FileInputStream fis = new FileInputStream(filePath);
+        Workbook wb = WorkbookFactory.create(fis);
+
+        System.out.println(wb.getSheet("Sayfa1").getRow(0).getCell(0));
+        System.out.println(wb.getSheet("Sayfa1").getRow(1).getCell(0));
+
+
+        String sifre = wb.getSheet("Sayfa1").getRow(0).getCell(0).toString();
+        String eskiSifre = wb.getSheet("Sayfa1").getRow(1).getCell(0).toString();
+        String bos = "";
+
+        bos = sifre;
+        sifre = eskiSifre;
+        eskiSifre = bos;
+
+        System.out.println("sifre = " + sifre);
+        System.out.println("eskiSifre = " + eskiSifre);
+
+        Sheet sheet = wb.getSheet("Sayfa1");
+        Row row = sheet.getRow(0);
+        Cell cell = row.getCell(0);
+        row.removeCell(cell);
+        FileOutputStream fos = new FileOutputStream(filePath);
+        wb.write(fos);
+
+
+        Sheet sheet1 = wb.getSheet("Sayfa1");
+        Row row2 = sheet1.getRow(1);
+        Cell cell2 = row2.getCell(0);
+        fos = new FileOutputStream(filePath);
+        row2.removeCell(cell2);
+        wb.write(fos);
+
+
+        wb.getSheet("Sayfa1").getRow(0).createCell(0).setCellValue(sifre);
+        wb.getSheet("Sayfa1").getRow(1).createCell(0).setCellValue(eskiSifre);
+        fos = new FileOutputStream(filePath);
+        wb.write(fos);
+
+        fos.close();
+        fis.close();
+        wb.close();
+    }
+
+    @And("kullanici new password box'a sifre girer")
+    public void kullaniciNewPasswordBoxASifreGirer() {
+
+        page.newPasswordBox.sendKeys("qwwertA!w");
+    }
+
+    @And("kullanici new password confirmation box'a new passworddan farkli sifre girer")
+    public void kullaniciNewPasswordConfirmationBoxANewPassworddanFarkliSifreGirer() {
+
+        page.confirmPasswordBox.sendKeys("qwrwett!A");
+        page.saveButton.click();
+    }
+
+    @And("kullanici {string} mesajinin gorunur oldugunu dogrular")
+    public void kullaniciMesajininGorunurOldugunuDogrular(String message) throws IOException {
+
+        Assert.assertTrue(page.notMatchTextMessage.isDisplayed());
+        ReusableMethods.getScreenshotWebElement("dontMatch", page.notMatchTextMessage);
+    }
+
+    @And("kullanici kucuk harf,buyuk harf,rakam ve ozel karakter ile en fazla alti karakterli new password girer")
+    public void kullaniciKucukHarfBuyukHarfRakamVeOzelKarakterIleEnFazlaAltiKarakterliNewPasswordGirer() {
+
+        page.newPasswordBox.sendKeys("A23!aa");
+    }
+
+    @And("kullanici Password strength seviyesinin red oldugunu dogrular")
+    public void kullaniciPasswordStrengthSeviyesininRedOldugunuDogrular() throws IOException {
+
+        Assert.assertEquals(1, page.redStrength.size());
+        ReusableMethods.getScreenshotWebElement("redStrength", page.strengthBar);
     }
 }
--- src/test/resources/Features/US008.feature
+++ src/test/resources/Features/US008.feature
@@ -13,40 +13,15 @@ Feature: US008 Kullanici giris yaptiginda Password sekmesi duzenlenebilir olmali
     And sayfayi kapatir
 
 
-  @TC002
-  Scenario: TC002 Daha guclu sifre icin en az 1 kucuk harf olmali ve "Password strength:" seviyesinin degistigini gorulebilmelidir.
+  @2345
+  Scenario Outline:TC002 TC003 TC004T C005 Sifre en az 1 buyuk harf icermeli ve "Password strength:" seviyesinin degistigi gorulebilmelidir.
     Given kullanici "medunnaUrl" url'e gider
     When kullanici login olur
     Then kullanici account menuden password link tiklar
     And kullanici current password girer
-    And kullanici buyuk harf,rakam ve ozel karakter ile en az yedi karakter new password girer
+    And kullanici kucuk harf,buyuk harf,rakam ve ozel karakter sartlarindan herhangi ucu ile"<new password>" girer
     And kullanici Password strenght seviyesinin limegreen oldugunu dogrular
-    And kullanici New password'a kucuk harf ekler
-    And kullanici Password strength seviyesinin green oldugunu dogrular
-    And sayfayi kapatir
-
-
-  @TC003
-  Scenario: Sifre en az 1 buyuk harf icermeli ve "Password strength:" seviyesinin degistigi gorulebilmelidir.
-    Given kullanici "medunnaUrl" url'e gider
-    When kullanici login olur
-    Then kullanici account menuden password link tiklar
-    And kullanici current password girer
-    And kullanici kucuk harf,rakam ve ozel karakter ile en az yedi karakter new password girer
-    And kullanici Password strenght seviyesinin limegreen oldugunu dogrular
-    And kullanici New password'a buyuk harf ekler
-    And kullanici Password strength seviyesinin green oldugunu dogrular
-    And sayfayi kapatir
-
-  @TC004
-  Scenario Outline: Sifre en az 1 buyuk harf icermeli ve "Password strength:" seviyesinin degistigi gorulebilmelidir.
-    Given kullanici "medunnaUrl" url'e gider
-    When kullanici login olur
-    Then kullanici account menuden password link tiklar
-    And kullanici current password girer
-    And kullanici kucuk harf,rakam ve ozel karakter ile en az yedi karakter "<new password>" girer
-    And kullanici Password strenght seviyesinin limegreen oldugunu dogrular
-    And kullanici "<New password'a>" buyuk harf ekler
+    And kullanici "<New password'a>" girmedigi sifre sartini ekler
     And kullanici Password strength seviyesinin green oldugunu dogrular
     And sayfayi kapatir
 
@@ -58,3 +33,59 @@ Feature: US008 Kullanici giris yaptiginda Password sekmesi duzenlenebilir olmali
       | AAAAssds12   | !!             |
 
 
+  @TC006
+  Scenario: TC006 Guclu bir parola icin sifre en az 7 karakterden olusmalidir
+    Given kullanici "medunnaUrl" url'e gider
+    When kullanici login olur
+    Then kullanici account menuden password link tiklar
+    And kullanici current password girer
+    And kullanici kucuk harf,buyuk harf,rakam ve ozel karakter ile en az yedi karakterli new password girer
+    And kullanici Password strength seviyesinin green oldugunu dogrular
+    And sayfayi kapatir
+
+
+  @TC007
+  Scenario: TC007 Eski sifre kullanilmamalidir
+    Given kullanici "medunnaUrl" url'e gider
+    When kullanici login olur
+    Then kullanici account menuden password link tiklar
+    And kullanici mevcut sifreyi current password box'a girer
+    And kullanici new password box'a eski sifreyi girer
+    And kullanici new password confirmation box'a ayni sifreyi girer
+    And kullanici save butonuna tiklar
+    And kullanici 4 saniye bekler
+    And kullanici Password changed! uyarisinin gorunur olmadigini dogrular
+    And sayfayi kapatir
+
+  @negativeTC001
+  Scenario: TC001 negatif "New password confirmation" onaylanabilmelidir
+    Given kullanici "medunnaUrl" url'e gider
+    When kullanici login olur
+    Then kullanici account menuden password link tiklar
+    And kullanici current password girer
+    And kullanici new password box'a sifre girer
+    And kullanici new password confirmation box'a new passworddan farkli sifre girer
+    And kullanici "The password and its confirmation do not match!" mesajinin gorunur oldugunu dogrular
+    And kullanici 2 saniye bekler
+    And sayfayi kapatir
+
+
+  @negative1_TC006
+  Scenario: TC006 negatif Guclu bir parola icin sifre en az 7 karakterden olusmalidir
+    Given kullanici "medunnaUrl" url'e gider
+    When kullanici login olur
+    Then kullanici account menuden password link tiklar
+    And kullanici current password girer
+    And kullanici kucuk harf,buyuk harf,rakam ve ozel karakter ile en fazla alti karakterli new password girer
+    And kullanici Password strength seviyesinin red oldugunu dogrular
+    And sayfayi kapatir
+
+
+  @negative2_TC006
+  Scenario: TC
+
+
+  @excel
+  Scenario:
+    Given excel test
+    And sayfayi kapatir
