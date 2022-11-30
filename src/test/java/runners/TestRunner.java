a0f1fb454a7e8b01d92bbfdc2bc3b1bae668ae28
cevik <suleymancevik64@gmail.com>
2022-11-30 20:56:48 +0300

    22-11-30

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
                 "junit:target/xml-report/cucumber.xml"},
         features = "src/test/resources/features",
         glue = "stepDefinitions",
-        tags = "@US014",
+        tags = "@db",
         dryRun = false
 )
 public class TestRunner {
